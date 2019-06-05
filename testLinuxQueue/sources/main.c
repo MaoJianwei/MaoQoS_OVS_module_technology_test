725343329e8c2fccd4c691c6350869e349c6954a
Jianwei Mao <maojianwei2020@gmail.com>
2019-06-05 18:36:23 +0800

    update when master graduation, 20190605

--- testLinuxQueue/sources/main.c
+++ testLinuxQueue/sources/main.c
@@ -1,6 +1,7 @@
 
 #include <malloc.h>
 #include <sys/queue.h>
+#include <string.h>
 
 #ifndef NULL
 #define NULL ((void *)0)
@@ -17,11 +18,68 @@ struct cmdQueueHead {
 	  struct cmdQueueEntry * *tqh_last;
 };
 
+/**
+ * cmd will be copied away, That release cmd is the invoker's duty
+ */
+void maoEnQueue(struct cmdQueueHead *cmdQueue, char * cmd){
+	struct cmdQueueEntry * data = (struct cmdQueueEntry * )calloc(1, sizeof(struct cmdQueueEntry));
+	data->cmdQos = (char*)calloc(1,strlen(cmd)+1); // +1 include \0
+	strcpy(data->cmdQos,cmd);
+	TAILQ_INSERT_TAIL(cmdQueue, data, tailq_entry);
+}
+
+/**
+ * That release cmd char* is the invoker's duty; The rest is my duty
+ * @Return : char*, will be NULL if Queue is empty
+ */
+char * maoDeQueue(struct cmdQueueHead *cmdQueue){
+
+	if(TAILQ_EMPTY(cmdQueue)){
+		return NULL;
+	}
+
+	struct cmdQueueEntry* DATA = TAILQ_FIRST(cmdQueue);
+
+	char * cmd = DATA->cmdQos;
+
+	TAILQ_REMOVE(cmdQueue, DATA, tailq_entry);
+	free(DATA);
+	DATA = NULL;
+
+	return cmd;
+}
+void maoInitQueue(struct cmdQueueHead * queue){
+
+	TAILQ_INIT(queue);
+}
+
+
 int main(){
 
 	struct cmdQueueHead * cmdQueue = (struct cmdQueueHead *)calloc(1,sizeof(struct cmdQueueHead));
-	TAILQ_INIT(cmdQueue);
+	maoInitQueue(cmdQueue);
 
+	char * beijing = "beijing";
+	char * qingdao = "qingdao";
+	char * haikou = "haikou";
+
+	maoEnQueue(cmdQueue, beijing);
+	maoEnQueue(cmdQueue, qingdao);
+	maoEnQueue(cmdQueue, haikou);
+
+	char * temp = NULL;
+	temp = maoDeQueue(cmdQueue);
+	free(temp);
+	temp = maoDeQueue(cmdQueue);
+	free(temp);
+	temp = maoDeQueue(cmdQueue);
+	free(temp);
+	temp = maoDeQueue(cmdQueue);
+	free(temp);
+
+
+
+	/*  below is original
 	struct cmdQueueEntry * data;
 
 	data = (struct cmdQueueEntry * )calloc(1, sizeof(struct cmdQueueEntry));
@@ -65,6 +123,7 @@ int main(){
 	if(TAILQ_EMPTY(cmdQueue)){
 		int aaa = 1;
 	}
+	*/
 
 	free(cmdQueue);
 	cmdQueue = NULL;
